caa72e47c1b62e3a7b33761131c9940eb3f38738
shohan777 <shohan.st27@gmail.com>
2019-10-26 22:29:52 +0600

    Added all assets and Make Home Component and add intro section

---
+++ src/views/Index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="index">
+        <section id="intro">
+            <div class="container-fluid">
+                <div class="row no-gutters">
+                    <div class="col">
+                        <div class="company-name"><span>B Wahlstroem</span> Engineering GmbH</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
